239ecda436e8ba2688cb01f5adec61175d9b994f
Noor <engrnoorahmed407@gmail.com>
2020-09-19 08:27:54 -0700

    Profile Updated and Routes Secure

--- frontend/auth/src/component/profile.js
+++ frontend/auth/src/component/profile.js
@@ -1,10 +1,32 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
 export class Profile extends Component {
+
+    state = {
+        userName: ''
+    }
+
+    componentDidMount = () =>{
+        const userData = JSON.parse(localStorage.getItem('user'));
+        if (userData){
+            this.setState({
+                userName: userData.user.name
+            });
+        } else{
+            this.props.history.push('/');
+        }
+    }
+    logout= e =>{
+        e.preventDefault();
+        localStorage.clear();
+        this.props.history.push('/');
+    }
     render() {
         return (
             <div>
-                <h1>Profile</h1>
+                <h1>Hello {this.state.userName}</h1>
+                <button 
+                onClick={this.logout}
+                className="btn btn-warning">Logout</button>
                 
             </div>
         )
